63f5ed9ef9085f9b0cbb0f80f695b1a598c28722
Dustin <dbouse4@wgu.edu>
2026-05-17 14:17:19 -0700

    first commit

---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsTwo: string[] = ["Golf", "Cricket", "Tennis"];
+
+sportsTwo.push("Baseball");
+sportsTwo.push("Futbol");
+
+for (let tempSort of sportsTwo) {
+    console.log(tempSort);
+}
